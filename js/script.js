b87f31e6000cbc9d28d5c44ec60bc017b5c73c43
Luca Giozza <luca.giozza@gmail.com>
2021-07-05 10:06:49 +0200

    push che non avevo pushato

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 const app = new Vue({
        el: '#app',
        data: {
-           email : ''
+           email : [],
 
 
        },
@@ -15,8 +15,8 @@ const app = new Vue({
             axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then (result => {
-                this.email = result.data.response
-                console.log(this.email);
+                console.log(result.data.response);
+                this.email.push(result.data.response);
                
                 
 
